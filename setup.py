1f462588fdd518efdbf852d131335291694c0c02
hugo <hjbello.wk@gmail.com>
2020-04-26 15:06:59 +0200

    fixed errors in configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="subject_classification_spanish",
-    version="0.0.2",
+    version="0.0.4",
     author="Hugo J. Bello",
     author_email="hjbello.wk@gmail.com",
     description="Subject and topic analysis for sentences in spanish",
